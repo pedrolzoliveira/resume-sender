cd0aaaaa2d0fc52fa8cc7bc54c7094adf32a16df
Pedro Oliveira <alvesp2001@gmail.com>
2022-06-13 04:10:34 -0300

    refactor(sendResume): aligning text

--- src/application/mailer/Mailer.ts
+++ src/application/mailer/Mailer.ts
@@ -9,10 +9,12 @@ export class Mailer {
             to: email,
             from: process.env.EMAIL_FROM,
             subject: process.env.EMAIL_SUBJECT,
-            text: `Olá, eu sou Pedro e vi que vocês estão precisando de um desenvolvedor NodeJS e eu gostaria de trabalhar com vocês!
-            Eu automatizei o processo de envio de emails com meu currículo para as vagas de NodeJS que foram publicadas no GitHub de vagas.
-            Se quiser dar uma olhada nesse processo, acesse o link abaixo:
-            https://www.github.com/pdroluiz/resume-sender`,
+            text: `
+Olá, eu sou Pedro e vi que vocês estão precisando de um desenvolvedor NodeJS e eu gostaria de trabalhar com vocês!
+Eu automatizei o processo de envio de emails com meu currículo para as vagas de NodeJS que foram publicadas no GitHub de vagas.
+Se quiser dar uma olhada nesse processo, acesse o link abaixo:
+
+https://www.github.com/pdroluiz/resume-sender`,
             attachments: [
                 {
                     filename: 'resume.pdf',
